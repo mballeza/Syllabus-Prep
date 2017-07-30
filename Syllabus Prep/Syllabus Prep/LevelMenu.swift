d7c2aabec7b0a6a39b87680156558c38d5330e6e
Matthew Balleza <matthewballeza@Laurens-MacBook-Pro-4.local>
2017-07-30 13:08:56 -0700

    Initial commit of UIDevelopment branch

---
+++ Syllabus Prep/Syllabus Prep/LevelMenu.swift
@@ -0,0 +1,38 @@
+//
+//  LevelMenu.swift
+//  Syllabus Prep
+//
+//  Created by Matthew Balleza on 7/24/17.
+//  Copyright © 2017 Matthew Balleza. All rights reserved.
+//
+
+import UIKit
+
+class LevelMenu: UIViewController {
+    var level = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Level " + String(level)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "segueEarTraining" {
+            let earTrainingController = segue.destination as! EarTrainingView
+            earTrainingController.viewTitle = "Ear Training"
+            earTrainingController.level = self.level
+        }
+    }
+
+}
